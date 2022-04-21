41750926583f8e190449a940a3e8f61fa1c17a67
Nick <Nicholasmaassen@yahoo.com>
2022-04-21 16:50:49 -0400

    Adds half of second round of ranking

--- src/components/RankStack.js
+++ src/components/RankStack.js
@@ -12,6 +12,7 @@ const RankStack = ({ topTraits, setTopTraits, history}) => {
 
     const [initialPairs, setInitialPairs] = React.useState([]);
     const [sortedPairs, setSortedPairs] = React.useState([]);
+    const [sortingPair, setSortingPair] = React.useState([]);
 
     const [joinStack, setJoinStack] = React.useState([]);
     const [mergeStack, setMergeStack] = React.useState([]);
@@ -31,41 +32,76 @@ const RankStack = ({ topTraits, setTopTraits, history}) => {
     },[])
 
     const handlePick = (pick) => {
-        console.log(pick)
+        console.log("pick: " + pick)
         if (initialPairs.length !== 0){
             doInitialRound(pick);
         }
-        console.log(initialPairs)
-        console.log(sortedPairs)
-        console.log(finishedList)
+        if (sortedPairs.length !== 0 && mergeStack.length === 0 && initialPairs.length === 0){
+            console.log("building round 2")
+            buildJoinStack(sortedPairs[0])
+            buildMergeStack(sortedPairs[0])
+            setSortingPair(sortedPairs[0])
+            setSortedPairs(sortedPairs.slice(2))
+        }
+        if(joinStack.length !== 0){
+            doJoinRound(pick)
+        }
+
+        console.log("initialPairs: ", initialPairs)
+        console.log("sortedPairs: ", sortedPairs)
+        console.log("finished list: ", finishedList)
     }
+
+
 
     const doInitialRound = (pick) => {
         let _initialPairs = initialPairs;
         let _sortedPairs = sortedPairs;
         if(pick === _initialPairs[0][0]){
             _initialPairs[0] = _initialPairs[0].reverse();
-            console.log("reversed" + _initialPairs[0])
+            console.log("reversed ", _initialPairs[0])
         }
 
         _sortedPairs.push(_initialPairs.shift());
 
         setDisplayedPairs(_initialPairs[0]);
-        if(initialPairs.length === 0){
+        if(initialPairs.length === 1){
             setFinishedList(_sortedPairs.shift());
+            console.log("Time for round 2")
         }
         setInitialPairs(_initialPairs);
         setSortedPairs(_sortedPairs);
     }
 
-    const buildJoinQueue = (list) =>{
-        let _joinStack = [];
+    const doJoinRound = (pick) => {
+        if(pick === sortingPair[0]){
+            setFinishedList(finishedList.concat(sortingPair))
+            clearStacks();
+        }
+        else if(pick === sortingPair[1]){
+            setFinishedList(sortingPair.concat(finishedList))
+            clearStacks();
+        }
+        let nextPair = joinStack.slice(1);
+        if(nextPair){
+            setJoinStack(nextPair);
+            setDisplayedPairs(nextPair)
+        }
 
-        joinStack.push([list[0], finishedList[finishedList.length-1]]);
-        joinStack.push([list[1],finishedList[0]]);
+
     }
 
-    const buildMergeQueue = (list) => {
+    const buildJoinStack = (list) =>{
+        let _joinStack = [];
+
+        _joinStack.push([list[0], finishedList[finishedList.length-1]]);
+        _joinStack.push([list[1],finishedList[0]]);
+        setJoinStack(_joinStack);
+        setDisplayedPairs(_joinStack[0])
+        console.log("Join stack: ", _joinStack)
+    }
+
+    const buildMergeStack = (list) => {
         let leftMergeStack = [];
         let rightMergeStack = [];
 
@@ -78,6 +114,10 @@ const RankStack = ({ topTraits, setTopTraits, history}) => {
         setMergeStack([leftMergeStack, rightMergeStack]);
     }
 
+    const clearStacks = () => {
+        setJoinStack([]);
+        setMergeStack([])
+    }
     return (
         <div>
             <Button value={displayedPairs[0]} onClick={e => handlePick(e.target.value)}>{displayedPairs[0]}</Button>
