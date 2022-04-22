f934df5fe246a0686e7eab948d9a1def8aeab283
Nick <Nicholasmaassen@yahoo.com>
2022-04-21 22:54:13 -0400

    Refactors rank to useRef. Breaks at merge phase

--- src/components/RankStack.js
+++ src/components/RankStack.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useRef} from "react";
 import Trait from "./Trait";
 import styled from "styled-components";
 import TraitCard from "./TraitCard";
@@ -10,114 +10,167 @@ import {Button} from "@mui/material";
 const RankStack = ({ topTraits, setTopTraits, history}) => {
     const [displayedPairs, setDisplayedPairs] = React.useState(topTraits.slice(0,2));
 
-    const [initialPairs, setInitialPairs] = React.useState([]);
-    const [sortedPairs, setSortedPairs] = React.useState([]);
-    const [sortingPair, setSortingPair] = React.useState([]);
+    let initialPairs = React.useRef([]);
+    let sortedPairs = React.useRef([]);
+    let sortingPair = React.useRef([]);
 
-    const [joinStack, setJoinStack] = React.useState([]);
-    const [mergeStack, setMergeStack] = React.useState([]);
+    let joinStack = React.useRef([]);
+    let mergeStack = React.useRef([]);
 
-    const [leftGuess, setLeftGuess] = React.useState(0);
-    const [rightGuess, setRightGuess] = React.useState(0);
+    let leftGuess = React.useRef(0);
+    let rightGuess = React.useRef(0);
 
-    const [finishedList, setFinishedList] = React.useState([]);
+    let finishedList = React.useRef([]);
 
     useEffect(() => {
-        let newInitialPairs = []
         for (let i = 0; i < topTraits.length; i += 2){
-            newInitialPairs.push([topTraits[i],topTraits[i+1]]);
+            initialPairs.current.push([topTraits[i],topTraits[i+1]]);
         }
-        setInitialPairs(newInitialPairs)
-
     },[])
 
     const handlePick = (pick) => {
         console.log("pick: " + pick)
-        if (initialPairs.length !== 0){
-            doInitialRound(pick);
-        }
-        if (sortedPairs.length !== 0 && mergeStack.length === 0 && initialPairs.length === 0){
-            console.log("building round 2")
-            buildJoinStack(sortedPairs[0])
-            buildMergeStack(sortedPairs[0])
-            setSortingPair(sortedPairs[0])
-            setSortedPairs(sortedPairs.slice(2))
-        }
-        if(joinStack.length !== 0){
+
+        if(joinStack.current.length !== 0){
             doJoinRound(pick)
         }
+        else if (mergeStack.current.length !== 0){
+            doMergeRound(pick)
+        }
 
-        console.log("initialPairs: ", initialPairs)
-        console.log("sortedPairs: ", sortedPairs)
-        console.log("finished list: ", finishedList)
+        if (initialPairs.current.length !== 0){
+            doInitialRound(pick);
+        }
+        if (sortedPairs.current.length !== 0 && mergeStack.current.length === 0 && initialPairs.current.length === 0){
+            doBuild(sortedPairs.current[0])
+        }
+
+
+        console.log("initialPairs: ", initialPairs.current)
+        console.log("sortedPairs: ", sortedPairs.current)
+        console.log("finished list: ", finishedList.current)
+        console.log("Join stack: ", joinStack.current)
+        console.log("mergeStack", mergeStack.current)
+
     }
 
 
 
     const doInitialRound = (pick) => {
-        let _initialPairs = initialPairs;
-        let _sortedPairs = sortedPairs;
-        if(pick === _initialPairs[0][0]){
-            _initialPairs[0] = _initialPairs[0].reverse();
-            console.log("reversed ", _initialPairs[0])
+
+        if(pick === initialPairs.current[0][0]){
+            initialPairs.current[0].reverse();
+            console.log("reversed ", initialPairs.current[0])
         }
 
-        _sortedPairs.push(_initialPairs.shift());
+        sortedPairs.current.push(initialPairs.current.shift());
 
-        setDisplayedPairs(_initialPairs[0]);
-        if(initialPairs.length === 1){
-            setFinishedList(_sortedPairs.shift());
-            console.log("Time for round 2")
+        setDisplayedPairs(initialPairs.current[0]);
+        if(initialPairs.current.length === 1){
+            finishedList.current = sortedPairs.current.shift();
+            console.log("Prepping for round 2")
         }
-        setInitialPairs(_initialPairs);
-        setSortedPairs(_sortedPairs);
     }
 
     const doJoinRound = (pick) => {
-        if(pick === sortingPair[0]){
-            setFinishedList(finishedList.concat(sortingPair))
+        if(pick === sortingPair.current[0]){
+            finishedList.current = finishedList.current.concat(sortingPair.current)
             clearStacks();
         }
-        else if(pick === sortingPair[1]){
-            setFinishedList(sortingPair.concat(finishedList))
+        else if(pick === sortingPair.current[1]){
+            finishedList.current = (sortingPair.current.concat(finishedList.current))
             clearStacks();
         }
-        let nextPair = joinStack.slice(1);
-        if(nextPair){
-            setJoinStack(nextPair);
-            setDisplayedPairs(nextPair)
+        else {
+            if (joinStack.current.length === 2) {
+                joinStack.current.shift();
+                setDisplayedPairs(joinStack.current[0])
+            }
+            else {
+                joinStack.current = [];
+                setDisplayedPairs(mergeStack.current[0][0])
+            }
         }
+    }
 
+    const doMergeRound = (pick) => {
 
+        if(displayedPairs[0] === mergeStack.current[0][0]) {
+            if (pick === displayedPairs[0]) {
+                finishedList.current.splice(leftGuess.current, 0, pick);
+                mergeStack.current[0] = [];
+            }
+            else {
+                mergeStack.current.shift();
+                leftGuess.current++;
+                console.log("leftGuess ", leftGuess.current);
+                if(mergeStack.current[0].length === 0){
+                    finishedList.current.splice(leftGuess.current, 0, pick);
+                }
+                if(mergeStack.current[1].length !== 0){
+                    setDisplayedPairs(mergeStack.current[1][0]);
+                }
+                else {
+                    setDisplayedPairs(mergeStack.current[0][0]);
+                }
+            }
+        }
+        else {
+            if(pick === displayedPairs[0]){
+                finishedList.current.splice(rightGuess.current, 0, pick);
+                mergeStack.current[1] = []
+            }
+            else {
+                mergeStack.current.shift();
+                rightGuess.current--;
+                console.log("rightGuess ", rightGuess.current);
+                if(mergeStack.current[1].length === 0){
+                    finishedList.current.splice(rightGuess.current, 0, pick);
+                }
+                if(mergeStack.current[0].length !== 0){
+                    setDisplayedPairs(mergeStack.current[0][0]);
+                }
+                else {
+                    setDisplayedPairs(mergeStack.current[1][0])
+                }
+            }
+        }
+    }
+
+    const doBuild = (list) => {
+        console.log("building round 2")
+        buildJoinStack(list);
+        buildMergeStack(list);
+        sortingPair.current = sortedPairs.current.shift();
+        leftGuess.current = 0;
+        rightGuess.current = finishedList.current.length;
     }
 
     const buildJoinStack = (list) =>{
-        let _joinStack = [];
-
-        _joinStack.push([list[0], finishedList[finishedList.length-1]]);
-        _joinStack.push([list[1],finishedList[0]]);
-        setJoinStack(_joinStack);
-        setDisplayedPairs(_joinStack[0])
-        console.log("Join stack: ", _joinStack)
+        joinStack.current.push([list[0], finishedList.current[finishedList.current.length-1]]);
+        joinStack.current.push([list[1],finishedList.current[0]]);
+        setDisplayedPairs(joinStack.current[0])
+        console.log("Initial Join stack: ", joinStack.current)
     }
 
     const buildMergeStack = (list) => {
         let leftMergeStack = [];
         let rightMergeStack = [];
 
-        for(let i = 0; i < finishedList.length; i++){
-            leftMergeStack.push([list[0],finishedList[i]]);
+        for(let i = 0; i < finishedList.current.length; i++){
+            leftMergeStack.push([list[0],finishedList.current[i]]);
         }
-        for(let i = finishedList.length - 1; i >= 0; i--){
-            rightMergeStack.push([list[1], finishedList[i]]);
+        for(let i = finishedList.current.length - 1; i >= 0; i--){
+            rightMergeStack.push([list[1], finishedList.current[i]]);
         }
-        setMergeStack([leftMergeStack, rightMergeStack]);
+        mergeStack.current = [leftMergeStack, rightMergeStack];
+        console.log("initial mergeStack", mergeStack.current)
+    }
+    const clearStacks = () => {
+        joinStack.current = [];
+        mergeStack.current = [];
     }
 
-    const clearStacks = () => {
-        setJoinStack([]);
-        setMergeStack([])
-    }
     return (
         <div>
             <Button value={displayedPairs[0]} onClick={e => handlePick(e.target.value)}>{displayedPairs[0]}</Button>
