62a52ca608cbb8ca6e0f8cead5b5c62803d0eceb
Nick <Nicholasmaassen@yahoo.com>
2022-04-29 12:48:29 -0400

    moves handle done functionality out of DoneButton.js

--- src/components/Selection/DoneButton.js
+++ src/components/Selection/DoneButton.js
@@ -1,50 +1,11 @@
 import React from "react";
 import {Button} from "@mui/material";
-import listOfAllTraits from "../../Assets/listOfAllTraits";
 
-const DoneButton = ({topTraits, setTopTraits, columnData, setColumnData, history}) => {
-    const allTraits = listOfAllTraits;
-    const [currentTraits, setCurrentTraits] = React.useState(allTraits.slice(0,10));
-
-
-    const handleDone = () => {
-        console.log(currentTraits);
-        if (currentTraits[currentTraits.length-1] !== allTraits[allTraits.length-1]){
-            addTopTraits(columnData.columns.column3.traitIds);
-            let indexOfLastTrait = allTraits.indexOf(currentTraits[currentTraits.length-1]);
-            let newTraits = allTraits.slice(indexOfLastTrait+1,indexOfLastTrait+11);
-            setCurrentTraits(newTraits);
-            clearColumns();
-            updateColumn(newTraits);
-            console.log(currentTraits);
-        }
-        else{
-            history.push("/Rank");
-        }
-    }
-    const addTopTraits = (traitsToAdd) => {
-        let newTopTraits = topTraits;
-        newTopTraits = newTopTraits.concat(traitsToAdd);
-        setTopTraits(newTopTraits);
-    }
-    const clearColumns = () => {
-        let newColumnData = columnData;
-        // eslint-disable-next-line no-unused-vars
-        for (const [key, value] of Object.entries(newColumnData.columns)){
-            value.traitIds = [];
-        }
-        setColumnData(newColumnData);
-    }
-
-    const updateColumn = (newTraits) => {
-        let newColumnData = columnData;
-        newColumnData.columns.column2.traitIds = newTraits;
-        setColumnData(newColumnData)
-    }
+const DoneButton = ({onClick}) => {
 
 
     return(
-        <Button onClick={handleDone} size='large'>
+        <Button onClick={onClick} size='large'>
             Done
         </Button>
     )
--- src/components/Selection/SelectionPage.js
+++ src/components/Selection/SelectionPage.js
@@ -3,11 +3,51 @@ import DoneButton from "./DoneButton"
 import Column from "./Column";
 import {DragDropContext} from "react-beautiful-dnd";
 import {Grid} from "@mui/material";
+import listOfAllTraits from "../../Assets/listOfAllTraits";
 
 
 
 
 const SelectionPage = ({columnData, onDragEnd, topTraits, setTopTraits, setColumnData, history}) =>{
+    const [currentTraits, setCurrentTraits] = React.useState(listOfAllTraits.slice(0,10));
+
+    const handleDone = () => {
+        console.log(currentTraits);
+        if (currentTraits[currentTraits.length-1] !== listOfAllTraits[listOfAllTraits.length-1]){
+            addTopTraits(columnData.columns.column3.traitIds);
+            let indexOfLastTrait = listOfAllTraits.indexOf(currentTraits[currentTraits.length-1]);
+            let newTraits = listOfAllTraits.slice(indexOfLastTrait+1,indexOfLastTrait+11);
+            setCurrentTraits(newTraits);
+            clearColumns();
+            updateColumn(newTraits);
+            console.log(currentTraits);
+        }
+        else{
+            history.push("/Rank");
+        }
+    }
+    const addTopTraits = (traitsToAdd) => {
+        let newTopTraits = topTraits;
+        newTopTraits = newTopTraits.concat(traitsToAdd);
+        setTopTraits(newTopTraits);
+    }
+    const clearColumns = () => {
+        let newColumnData = columnData;
+        // eslint-disable-next-line no-unused-vars
+        for (const [key, value] of Object.entries(newColumnData.columns)){
+            value.traitIds = [];
+        }
+        setColumnData(newColumnData);
+    }
+
+    const updateColumn = (newTraits) => {
+        let newColumnData = columnData;
+        newColumnData.columns.column2.traitIds = newTraits;
+        setColumnData(newColumnData)
+    }
+
+
+
 
     return(
         <div>
@@ -31,11 +71,7 @@ const SelectionPage = ({columnData, onDragEnd, topTraits, setTopTraits, setColum
             >
                 <Grid item >
                     <DoneButton
-                        topTraits={topTraits}
-                        setTopTraits={setTopTraits}
-                        columnData={columnData}
-                        setColumnData={setColumnData}
-                        history={history}/>
+                       onClick={handleDone}/>
                 </Grid>
             </Grid>
 
