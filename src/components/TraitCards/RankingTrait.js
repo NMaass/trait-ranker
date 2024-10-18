9728fbbe66b3f4e7093c1dd1c2bf3aaa657b86db
Nick <Nicholasmaassen@yahoo.com>
2024-10-18 14:51:27 -0400

    Adds animations and definitions to ranking

--- src/components/TraitCards/RankingTrait.js
+++ src/components/TraitCards/RankingTrait.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../../style/CardStyle.scss";
-import { traitIcons } from "../../utils/listOfAllTraits";
+import { traitIcons, traitDefinitions } from "../../utils/listOfAllTraits";
 import { IconContext } from "react-icons";
-import { useMediaQuery, Grid } from "@mui/material";
+import { useMediaQuery, Grid, Typography } from "@mui/material";
 import TouchRipple from "@mui/material/ButtonBase/TouchRipple";
+import CardHelp from "./CardHelp";
 
-const RankingTrait = ({ trait, onClick }) => {
-  const isMobile = useMediaQuery("(min-width:1024px");
+const RankingTrait = ({ trait, onClick, className }) => {
+  const isMobile = useMediaQuery("(max-width:1024px)");
+  const [flipped, setFlipped] = useState(false);
 
   const rippleRef = React.useRef(null);
   const onRippleStart = (e) => {
@@ -16,30 +18,58 @@ const RankingTrait = ({ trait, onClick }) => {
     rippleRef.current.stop(e);
   };
 
+  const toggleFlipped = (e) => {
+    setFlipped(!flipped);
+    e.stopPropagation(); // Prevent the onClick from triggering
+  };
+
   return (
     <div
-      className="card rankCard"
+      className={`card rankCard ${flipped ? "flipped" : ""} ${className || ""}`}
       onClick={onClick}
       onMouseDown={onRippleStart}
       onMouseUp={onRippleStop}
     >
-      <Grid container
-            alignItems='center'
-            justifyContent='center'
-            direction="column">
-        <Grid item>
-      <h1>{trait}</h1>
-        </Grid>
-        <Grid item>
-
-
-      <IconContext.Provider
-        value={isMobile ? { size: "6vw" } : { size: "20vw" }}
-      >
-        {traitIcons[trait]}
-      </IconContext.Provider>
-        </Grid>
-      </Grid>
+      <div className="card-inner">
+        {/* Front Side */}
+        <div className="card-front">
+          <CardHelp toggleFlipped={toggleFlipped} flipped={flipped} />
+          <Grid
+            container
+            alignItems="center"
+            justifyContent="center"
+            direction="column"
+          >
+            <Grid item>
+              <h1>{trait}</h1>
+            </Grid>
+            <Grid item>
+              <IconContext.Provider
+                value={isMobile ? { size: "30vw" } : { size: "6vw" }}
+              >
+                {traitIcons[trait]}
+              </IconContext.Provider>
+            </Grid>
+          </Grid>
+        </div>
+        {/* Back Side */}
+        <div className="card-back">
+          <CardHelp toggleFlipped={toggleFlipped} flipped={flipped} />
+          <Grid
+            container
+            alignItems="center"
+            justifyContent="center"
+            direction="column"
+          >
+            <Grid item>
+              <h1>{trait}</h1>
+            </Grid>
+            <Grid item>
+              <Typography variant="h5">{traitDefinitions[trait]}</Typography>
+            </Grid>
+          </Grid>
+        </div>
+      </div>
       <TouchRipple ref={rippleRef} center={false} />
     </div>
   );
