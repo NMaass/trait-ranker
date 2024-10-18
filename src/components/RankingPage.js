eec2ab8a71601f381040fc86e3fe6bc7522de8bf
Nick <Nicholasmaassen@yahoo.com>
2024-10-18 14:52:08 -0400

    Applies new animations to ranking

--- src/components/RankingPage.js
+++ src/components/RankingPage.js
@@ -1,10 +1,18 @@
-import React, { useContext, useEffect, useMemo } from "react";
+import React, {
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+  useCallback,
+} from "react";
 import RankingTrait from "./TraitCards/RankingTrait";
-import { Grid, useMediaQuery, Button } from "@mui/material";
+import { Grid, useMediaQuery } from "@mui/material";
 import { ProgressContext } from "./App";
 import useMergeSort from "../utils/useMergeSort";
+import { UndoContext } from "./App";
+import "../style/CardStyle.scss";
 
-const RankingPage = ({ topTraits, history, finalList }) => {
+const RankingPage = ({ topTraits, setTopTraits, history, initalProgress }) => {
   // Memoize topTraits to prevent unnecessary re-initialization
   const memoizedTopTraits = useMemo(() => topTraits.slice(), [topTraits]);
   useEffect(() => {
@@ -13,27 +21,52 @@ const RankingPage = ({ topTraits, history, finalList }) => {
 
   const {
     progressPercent,
-    currentStanding,
     currentMatch,
+    currentStanding,
     matchWin,
     revertMatch,
     isComplete,
-  } = useMergeSort(memoizedTopTraits);
+  } = useMergeSort(memoizedTopTraits, initalProgress?.data?.ranking);
 
   const isMobile = useMediaQuery("(min-width:1024px)");
 
   const { progress, activeStep } = useContext(ProgressContext);
   const [progressState, setProgressState] = progress;
   const [activeStepState, setActiveStepState] = activeStep;
+  const [leftCardClass, setLeftCardClass] = useState("");
+  const [rightCardClass, setRightCardClass] = useState("");
+  const handleRoundWin = useCallback(
+    (trait) => {
+      // Trigger slide-out animation
+      if (trait === currentMatch.left) {
+        setLeftCardClass("slide-out");
+        setRightCardClass("fade-out");
+      } else {
+        setLeftCardClass("fade-out");
+        setRightCardClass("slide-out");
+      }
 
-  const handleRoundWin = (trait) => {
-    matchWin(trait);
-    setProgressState(progressPercent);
-  };
+      // Set timeout to update the match and reset animations after slide-out is complete
+      setTimeout(() => {
+        matchWin(trait);
+        setProgressState(progressPercent);
+        setLeftCardClass("slide-in");
+        setRightCardClass("slide-in");
+
+        // Set another timeout to reset the classes after the slide-in animation
+        setTimeout(() => {
+          setLeftCardClass("");
+          setRightCardClass("");
+        }, 600); // Timeout matches the slide-in animation duration
+      }, 600); // Timeout matches the slide-out animation duration
+    },
+    [currentMatch, matchWin, progressPercent, setProgressState]
+  );
 
   useEffect(() => {
     console.log("isComplete:", isComplete);
     if (isComplete) {
+      setTopTraits(currentStanding);
       setActiveStepState(3);
       history.push("/Results");
     }
@@ -61,16 +94,20 @@ const RankingPage = ({ topTraits, history, finalList }) => {
         {currentMatch && currentMatch.left && (
           <Grid item>
             <RankingTrait
+              className={leftCardClass}
               trait={currentMatch.left}
               onClick={() => handleRoundWin(currentMatch.left)}
+              key={currentMatch.left}
             />
           </Grid>
         )}
         {currentMatch && currentMatch.right && (
           <Grid item>
             <RankingTrait
+              className={rightCardClass}
               trait={currentMatch.right}
               onClick={() => handleRoundWin(currentMatch.right)}
+              key={currentMatch.right}
             />
           </Grid>
         )}
